4d166a1b5c62847640b0d160f1fdf063d856ddb2
Ysw <202021079@sangmyung.kr>
2025-01-09 23:35:53 +0900

    Memory DataBase CRUD 적용 - 3

--- src/main/java/com/example/memorydb/user/controller/UserApiController.java
+++ src/main/java/com/example/memorydb/user/controller/UserApiController.java
@@ -4,7 +4,9 @@ import com.example.memorydb.user.model.UserEntity;
 import com.example.memorydb.user.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,20 @@ public class UserApiController {
     @GetMapping("all")
     public List<UserEntity> findAll(){
         return userService.findAll();
+    }
+
+    @DeleteMapping("/id/{id}")
+    public void delete(
+            @PathVariable Long id
+    ){
+        userService.delete(id);
+    }
+
+    @GetMapping("/id/{id}")
+    public UserEntity findOne(
+            @PathVariable Long id
+    ){
+        var response = userService.findById(id);
+        return response.get();
     }
 }
--- src/main/java/com/example/memorydb/user/service/UserService.java
+++ src/main/java/com/example/memorydb/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.memorydb.user.service;
 import com.example.memorydb.user.db.UserRepository;
 import com.example.memorydb.user.model.UserEntity;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,4 +24,11 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+
+    public Optional<UserEntity> findById(Long id) {
+        return userRepository.findById(id);
+    }
 }
