847a61c6b0aeb73b1e3c66442a7dfb8a3ab423d2
Ysw <202021079@sangmyung.kr>
2025-01-12 23:26:36 +0900

    Memory DataBase CRUD 적용 - 6

--- src/main/java/com/example/memorydb/book/controller/BookApiController.java
+++ src/main/java/com/example/memorydb/book/controller/BookApiController.java
@@ -2,9 +2,12 @@ package com.example.memorydb.book.controller;
 
 import com.example.memorydb.book.db.entity.BookEntity;
 import com.example.memorydb.book.service.BookService;
+import com.example.memorydb.user.model.UserEntity;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,20 @@ public class BookApiController {
     @GetMapping("/all")
     public List<BookEntity> findAll(){
         return bookService.findAll();
+    }
+
+    @DeleteMapping("/id/{id}")
+    public void delete(
+            @PathVariable Long id
+    ){
+        bookService.delete(id);
+    }
+
+    @GetMapping("/id/{id}")
+    public BookEntity findOne(
+            @PathVariable Long id
+    ){
+        var response = bookService.findById(id);
+        return response.get();
     }
 }
--- src/main/java/com/example/memorydb/book/service/BookService.java
+++ src/main/java/com/example/memorydb/book/service/BookService.java
@@ -3,7 +3,9 @@ package com.example.memorydb.book.service;
 
 import com.example.memorydb.book.db.entity.BookEntity;
 import com.example.memorydb.book.db.repository.BookRepository;
+import com.example.memorydb.user.model.UserEntity;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,13 @@ public class BookService {
     //all
     public List<BookEntity> findAll() {
         return bookRepository.findAll();
+    }
+
+    //delete
+    public void delete(Long id) {bookRepository.delete(id);}
+
+    //one
+    public Optional<BookEntity> findById(Long id) {
+        return bookRepository.findById(id);
     }
 }
