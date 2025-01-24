796bcefa7d50e57733b780b0ee5608998b78e6b9
Ysw <202021079@sangmyung.kr>
2025-01-24 15:44:00 +0900

    Memory DB를 MYSQL로 변환 - nativeQuery 이용

--- src/main/java/com/example/memorydb/user/db/UserRepository.java
+++ src/main/java/com/example/memorydb/user/db/UserRepository.java
@@ -5,6 +5,7 @@ import com.example.memorydb.user.model.UserEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
@@ -15,8 +16,16 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     // select * from user where score >= ?? AND score <= ??
     List<UserEntity> findAllByScoreGreaterThanEqualAndScoreLessThanEqual(int min, int max);
 
+//    @Query(
+//           "select * from user as u where u.score >= ?1 AND u.score <= ?2"
+//    )
+//    List<UserEntity> score(int min, int max);
+
     @Query(
-            "select u from user u where u.score >= ?1 AND u.score <= ?2"
+            value = "select * from user as u where u.score >= :min AND u.score <= :max",
+            nativeQuery = true
     )
-    List<UserEntity> score(int min, int max);
+    List<UserEntity> score(
+            @Param(value = "min") int min,
+            @Param(value = "max") int max);
 }
